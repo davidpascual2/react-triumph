aa8b07acce82cfc086bac4b1c064d28dba696e34
David Pascual <davidpascual@Davids-MacBook-Pro.local>
2023-04-21 19:48:03 -0700

    fixed carousel

--- client/src/components/container1/navbar/Navbar.js
+++ client/src/components/container1/navbar/Navbar.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 import manhiking from '../../../assets/manhiking.webp';
-import mountainImage1 from '../../../assets/mountains-bg.jpg';
-import mountainImage2 from '../../../assets/mountains5.jpeg';
-import mountainImage3 from '../../../assets/snowy2.jpeg';
-import biblestudy2 from '../../../assets/biblestudy2.jpg';
+import biblestudy3 from '../../../assets/biblestudy3.jpeg';
+import activities from "../../../assets/activities2.jpeg";
 import triumph from '../../../assets/logo.001.beige.PNG';
+import triumphFB from '../../../assets/triumph-fb.png';
+import triumphIG from '../../../assets/triumphIG.png';
 
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
@@ -20,7 +20,7 @@ const settings = {
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 10000,
     // Additional settings as needed
   };
 
@@ -94,28 +94,28 @@ export default function Navbar() {
                         {/* Render your carousel slides here */}
                         <div className='slide1'>
                             
-                            <img className="carimage" src={biblestudy2} alt='example1'/>
+                            <img className="carimage" src={biblestudy3} alt='example1'/>
                             
                             <div className='slide-desc'>
                                 <h2>Bible Studies</h2>
-                                <p>exampel here hello there in there </p>
+                                <p>Bible studies are held every friday at 7pm. </p>
                             </div>
                             
                         </div>
 
                         <div className='slide1'>
-                            <img className="carimage" src={manhiking} alt='example1'/>
+                            <img className="carimage" src={activities} alt='example1'/>
                             <div className='slide-desc'>
                                 <h2>Activities</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolorum beatae nesciunt perspiciatis doloribus accusantium iusto officiis, Qui. </p>
+                                <p>Join us ice skating this weekend 4/23/23 at skate king! </p>
                             </div>
                         </div>
 
                         <div className='slide1'>
-                            <img className="carimage" src={mountainImage2} alt='example1'/>
+                            <img className="carimage" src={triumphIG} alt='example1'/>
                             <div className='slide-desc'>
                                 <h2>Follow us!</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur m, laudantium voluptatem delectus repellat autem placeat animi adipisci nesciunt, reiciendis, molestias ullam fuga ipsum assumenda mollitia. </p>
+                                <p> Follow us on Instagram and Facebook to stay up to date on church activties and connect with other members </p>
                             </div>
                         </div>
 
--- client/src/components/container2/about/About.js
+++ client/src/components/container2/about/About.js
@@ -11,9 +11,10 @@ export default function About() {
             {/* <!-- <div class="photo-container"> --> */}
                 <img className="triumph-pic" src={triumphGroup}/>
             {/* <!-- </div> --> */}
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus id ratione aspernatur aliquid! A consequatur excepturi deserunt est omnis placeat veniam autem illo quam vitae, maxime magnam repellendus, quasi esse!</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium eum cumque facere eos laborum natus, illum maxime, ut at, atque quas voluptate dolorum corrupti. Earum error esse unde vero distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda, vero temporibus pariatur fugit praesentium, veritatis molestias optio possimus vel similique odit culpa eius obcaecati delectus totam excepturi quis?</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed ad molestias rerum, pariatur similique provident unde nisi. Praesentium at nulla accusantium voluptate, animi repellat, aut, repudiandae nesciunt eligendi officiis illum!</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus id ratione aspernatur aliquid! A consequatur excepturi deserunt est omnis placeat veniam autem illo quam vitae,
+               maxime magnam repellendus, quasi esse! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates sit aut, veritatis quos hic a asperiores molestias rem deleniti earum.
+                Tempore suscipit est consectetur veniam atque eligendi exercitationem quam numquam.</p>
+            
         </div>
     </>
   )
